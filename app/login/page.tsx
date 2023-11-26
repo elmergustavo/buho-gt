87e89f247deefc3dad04333c4e91060a38f40dc5
Gustavo Pu <elmergustavo79@gmail.com>
2023-11-26 16:25:47 -0600

    update login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,18 @@
+"use client";
+
 import { Login } from "@/components/login/Login";
-import React from "react";
+// import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import React, { useState } from "react";
 
 const page = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <>
       <div className="flex  lg:flex-row min-h-screen">
@@ -18,7 +27,7 @@ const page = () => {
           />
         </div>
         {/* Segunda columna */}
-        <div className="w-full md:w-3/4 p-8 flex items-center justify-center">
+        <div className="w-full md:w-3/4 sm:p-8 p-4 flex items-center justify-center">
           <section className="w-full flex justify-center items-center">
             <div className="flex flex-col items-start w-full sm:max-w-2xl">
               <span className="px-8"></span>
@@ -37,7 +46,6 @@ const page = () => {
               <div className="w-full bg-white rounded-lg sm:p-8">
                 <div className="">
                   <div className="text-xl font-extrabold leading-tight tracking-tight text-gray-900 md:text-3xl lg:text-4xl space-y-6">
-                    
                     <Link href="/">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -57,22 +65,30 @@ const page = () => {
                     <p className="mb-4">Bienvenido a</p>
                     Buhogt.com
                   </div>
+                  <div className="flex justify-center items-center sm:hidden">
+                    <Image
+                      alt="buho logo"
+                      src={"/assets/logo/logo_horizontal.svg"}
+                      width={200}
+                      height={200}
+                    />
+                  </div>
 
                   <p className="my-4 opacity-70">Ingresa a tu cuenta</p>
                   <form className="space-y-4 md:space-y-6 my-4" action="#">
                     <div className="mb-4">
                       <label
                         htmlFor="email"
-                        className="mb-2.5 block font-medium text-black "
+                        className="mb-2.5 block font-medium text-black text-sm md:text-base"
                       >
-                        Email
+                        Correo Electronico
                       </label>
                       <div className="relative">
                         <input
                           type="email"
                           id="email"
                           placeholder="Enter your email"
-                          className="w-full rounded-lg border border-stroke bg-transparent py-3 pl-4 pr-4 outline-none focus:ring-primary-600 focus:border-primary-600 focus-visible:shadow-none "
+                          className="w-full rounded-lg border border-stroke bg-transparent py-3 pl-4 pr-4 outline-none focus:border-primary focus:outline-none focus:ring-primary-600 focus:border-primary-600 focus-visible:shadow-none "
                         />
 
                         <span className="absolute right-4 top-4"></span>
@@ -81,34 +97,72 @@ const page = () => {
                     <div className="mb-4">
                       <label
                         htmlFor="password"
-                        className="mb-2.5 block font-medium text-black "
+                        className="mb-2.5 block font-medium text-black text-sm md:text-base"
                       >
                         Contraseña
                       </label>
                       <div className="relative">
                         <input
+                          type={showPassword ? "text" : "password"}
                           id="password"
-                          type="password"
+                          name="password"
+                          value={password}
                           placeholder="Enter your email"
-                          className="w-full rounded-lg border border-stroke bg-transparent py-3 pl-6 pr-10 outline-none focus:ring-primary-600 focus:border-primary-600 focus-visible:shadow-none"
+                          onChange={(e) => setPassword(e.target.value)}
+                          className="w-full rounded-lg border border-stroke bg-transparent py-3 pl-4 pr-4 outline-none focus:border-primary focus:outline-none focus:ring-primary-600 focus:border-primary-600 focus-visible:shadow-none "
                         />
 
-                        <span className="absolute right-4 top-4"></span>
+                        <button
+                          type="button"
+                          onClick={togglePasswordVisibility}
+                          className="absolute top-3 right-4 cursor-pointer"
+                        >
+                          {showPassword ? (
+                            <>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                                />
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                />
+                              </svg>
+                            </>
+                          ) : (
+                            <>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                                />
+                              </svg>
+                            </>
+                          )}
+                        </button>
                       </div>
                     </div>
+
                     <div className="flex items-center justify-between">
                       <div className="flex items-start">
-                        {/* <div className="flex items-center h-5">
-                          <input
-                            id="remember"
-                            aria-describedby="remember"
-                            type="checkbox"
-                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 "
-                          />
-                        </div> */}
-                        {/* <div className="ml-3 text-sm">
-                          <label className="text-gray-500">Remember me</label>
-                        </div> */}
                       </div>
                       <a
                         href="#"
